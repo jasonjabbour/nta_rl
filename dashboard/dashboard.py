45153bc124aba0447a87e54a4a688b64dfea5a72
jasonjabbour <jjj4se@virginia.edu>
2022-08-03 02:02:08 -0700

    more dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def dashboard_home():
-    return render_template('index.html')
+    return render_template('index.html',
+                           network_traffic=[1]*15, 
+                           rl_confidence=[2]*15, 
+                           true_network_state=1, 
+                           rl_alert=1)
 
 if __name__ == '__main__':
     app.run(debug=True)
